54e6f3fd69f3ee53bf60d09fd1f08225c8403a2d
Hüseyin Emre Ergin <100281013+huseyineergin@users.noreply.github.com>
2025-05-06 16:26:27 +0300

    remove ebs

--- modules/terraform-aws-ebs/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "id" {
-  description = "The volume ID"
-  value       = try(aws_ebs_volume.this.id, null)
-}
-
-output "arn" {
-  description = "The volume ARN"
-  value       = try(aws_ebs_volume.this.arn, null)
-}
